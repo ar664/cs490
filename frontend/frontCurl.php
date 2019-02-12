edd94986f04edec0cbc24cf05caffd377f0e9106
SangMNguyen <44916061+SangMNguyen@users.noreply.github.com>
2019-02-12 11:59:56 -0500

    Added HTML Page + PHP curl from Alpha Stage

---
+++ frontend/frontCurl.php
@@ -0,0 +1,27 @@
+<?php
+    if(isset($_POST["Username"]) && isset($_POST["Password"])) { // If the formatting ain't pristine then it's a bad request
+        $req = curl_init();
+
+        $us = $_POST["Username"];
+        $pw = $_POST["Password"];
+        
+        curl_setopt($req, CURLOPT_URL,"https://web.njit.edu/~amp85/middleCurlsRevised.php");
+        curl_setopt($req, CURLOPT_POST, 1);
+        curl_setopt($req, CURLOPT_POSTFIELDS,"Username=" . $us . "&Password=" . $pw);
+
+        curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
+
+        $output = curl_exec($req);
+        if(empty($output)) { // If the error is related to php
+            $server_output = "PHP Error.";
+        }
+        else {
+            $server_output = $output;
+        }
+        curl_close($req);
+    }
+    else {
+        $server_output = "Bad request.";
+    }
+    echo $server_output;
+?>
