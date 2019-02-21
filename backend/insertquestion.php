f1f405907cef64d11ce7092268c5ba81f6bcd656
ar664 <ar664@njit.edu>
2019-02-21 15:50:31 -0500

    insertquestion now returns the correct format

--- backend/insertquestion.php
+++ backend/insertquestion.php
@@ -27,6 +27,22 @@ if($error)
     echo "SQL ERROR: " . $error;
 }
 
+$sql="SELECT * from Questions";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    //output data of each row
+    global $table;
+    $table = '{ "Test":"Test"';
+    while($row = mysqli_fetch_assoc($result)) {
+        $table = $table . ', {"ID": ' . $row["ID"] . ' , "Question":"' . $row["Question"] . '" , "Answer":"' . $row["Answer"] . '" , "Difficulty":"' . $row["Difficulty"] . '" , "TestCases":' . $row["TestCases"] . ' , "Points":' . $row["Points"] . '}';
+    }
+    $table = $table . '}';
+    echo $table;
+} else {
+    echo "0 results";
+}
+
 mysqli_close($conn);
 
 ?>
