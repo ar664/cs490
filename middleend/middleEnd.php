1ef68735754a62c9d34d0c2f2a6c2eb4c8c3fe00
ar664 <ar664@njit.edu>
2019-03-05 15:02:42 -0500

    Added queries to middleEnd. Simplified login for middleEnd.

--- middleend/middleEnd.php
+++ middleend/middleEnd.php
@@ -4,14 +4,6 @@ $username = $_POST['Username'];
 $password = $_POST['Password'];
 
 if (isset($username) && isset($password)){
-	class Student {
-		function Student() {
-		$this->dbSuccess = false;
-		$this->dbType="student";
-	}  
-    }
-
-	$Dummy = new Student();   
 
 	// MAKE DB Login Call
 	$url1 = "https://web.njit.edu/~ar664/cs490/backend/connect.php";
@@ -22,13 +14,42 @@ if (isset($username) && isset($password)){
 	curl_setopt($ch1, CURLOPT_RETURNTRANSFER, TRUE); // store result in var
 	$db_result = curl_exec($ch1);
 
-	$db_json_res = json_decode($db_result, TRUE);
-	$Dummy->dbSuccess = $db_json_res['dbSuccess'];
-	$Dummy->dbType= $db_json_res['dbType'];
- 	echo json_encode($Dummy);
+ 	echo $db_result;
 	curl_close($ch1);
 
 	//Tell me your Account Type
+
+} else if(isset($_POST["query"])) {
+    $query = $_POST["query"];
+    $url = "https://web.njit.edu/~ar664/cs490/backend/getquestions.php";
+    if($query == "GetQuestions") {
+        $url = $url;
+    } else if ($query == "InsertQuestion") {
+        $url = "https://web.njit.edu/~ar664/cs490/backend/insertquestion.php";
+    } else if ($query == "DeleteQuestion") {
+        $url = "https://web.njit.edu/~ar664/cs490/backend/deletequestion.php";
+    } else if ($query == "GetExam") {
+        $url = "https://web.njit.edu/~ar664/cs490/backend/getexam.php";
+    } else if ($query == "InsertExamQuestion") {
+        $url = "https://web.njit.edu/~ar664/cs490/backend/insertexamquestion.php";
+    } else if ($query == "UpdateExamQuestion") {
+        $url = "https://web.njit.edu/~ar664/cs490/backend/updateexamquestions.php";
+    } else if ($query == "DeleteExamQuestion") {
+        $url = "https://web.njit.edu/~ar664/cs490/backend/deleteexamquestions.php";
+    } else {
+        echo "ERROR: Query not found";
+    }
+    $ch1 = curl_init($url1);
+
+	curl_setopt($ch1, CURLOPT_POST, TRUE);
+	curl_setopt($ch1, CURLOPT_POSTFIELDS, http_build_query($_POST));
+	curl_setopt($ch1, CURLOPT_RETURNTRANSFER, TRUE); // store result in var
+    
+    $db_result = curl_exec($ch1);
+
+    echo $db_result;
+
+    curl_close($ch1);
 
 } else {
 	echo "Username and Password not set in POST FIELDS\n";
