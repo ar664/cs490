a32f80e004c86992d4d4bb971109268487c350b3
Sang <sangnguyenpublic@gmail.com>
2019-03-07 14:48:45 -0500

    Query, Add Questions, Reset Form, and Dynamic TestCases Work!

--- frontend/betaFrontCurl.php
+++ frontend/betaFrontCurl.php
@@ -1,11 +1,29 @@
 <?php
-    if(isset($_POST["username"]) && isset($_POST["password"])) { // If the formatting ain't pristine then it's a bad request
+    if(isset($_POST["query"])) {
         $req = curl_init();
 
-        $us = $_POST["Username"];
-        $pw = $_POST["Password"];
+        curl_setopt($req, CURLOPT_URL,"https://web.njit.edu/~ar664/cs490/middleend/middleEnd.php");
+        curl_setopt($req, CURLOPT_POST, 1);
+        curl_setopt($req, CURLOPT_POSTFIELDS, http_build_query($_POST));
+
+        curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
+
+        $output = curl_exec($req);
+        if(empty($output)) { // If there is no response
+            $server_output = "Empty result.";
+        }
+        else {
+            $server_output = $output;
+        }
+        curl_close($req);
+    }
+    else if(isset($_POST["username"]) && isset($_POST["password"])) { // If the formatting ain't pristine then it's a bad request
+        $req = curl_init();
+
+        $us = $_POST["username"];
+        $pw = $_POST["password"];
         
-        curl_setopt($req, CURLOPT_URL,"https://web.njit.edu/~amp85/middleCurlsRevised.php");
+        curl_setopt($req, CURLOPT_URL,"https://web.njit.edu/~ar664/cs490/middleend/middleEnd.php");
         curl_setopt($req, CURLOPT_POST, 1);
         curl_setopt($req, CURLOPT_POSTFIELDS,"Username=" . $us . "&Password=" . $pw);
 
@@ -20,61 +38,6 @@
         }
         curl_close($req);
     }
-
-    else if(isset($_POST["question"]) && isset($_POST["difficulty"]) && isset($_POST["testCases"]) && isset($_POST["topic"])) {
-        $req = curl_init();
-
-        $q = $_POST["question"];
-        $diff = $_POST["difficulty"];
-        $tc = $_POST["testCases"];
-        $top = $_POST["topic"];
-        
-        curl_setopt($req, CURLOPT_URL,"https://web.njit.edu/~amp85/middleCurlsRevised.php");
-        curl_setopt($req, CURLOPT_POST, 1);
-        curl_setopt($req, CURLOPT_POSTFIELDS,"Question=" . $q . "&Difficulty=" . $diff . "&TestCases=" . $tc . "&Topic=" . $top);
-
-        curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
-
-        $output = curl_exec($req);
-        if(empty($output)) { // If there is no response
-            $server_output = "Empty result.";
-        }
-        else {
-            $server_output = $output;
-        }
-        curl_close($req);
-    }
-
-    else if(isset($_POST["query"])) {//isset($_POST["keyword"]) || (isset($_POST["topic"])) || (isset($_POST["difficulty"]))) {
-        $req = curl_init();
-        
-        $fields = "";
-
-        if(isset($_POST["keyword"])) {
-            $fields = $fields . "Keyword=" . $_POST["keyword"];
-        }
-        if(isset($_POST["topic"]) && strlen($fields) > 0) {
-            $fields = $fields . "Topic=" . $_POST["topic"];
-        }
-        if(isset($_POST["difficulty"]) && strlen($fields) > 0) {
-            $fields = $fields . "Difficulty=" . $_POST["difficulty"];
-        }
-
-        curl_setopt($req, CURLOPT_URL,"https://web.njit.edu/~amp85/middleCurlsRevised.php");
-        curl_setopt($req, CURLOPT_POST, 1);
-        curl_setopt($req, CURLOPT_POSTFIELDS, $fields);
-
-        curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
-        $output = curl_exec($req);
-        if(empty($output)) { // If there is no response
-            $server_output = "Empty result.";
-        }
-        else {
-            $server_output = $output;
-        }
-        curl_close($req);
-    }
-
     else {
         $server_output = "Bad request.";
     }
--- frontend/search.js
+++ frontend/search.js
@@ -3,40 +3,50 @@ var searchKeyword = true;
 var searchTopic = false;
 var searchDifficulty = false;
 var numCases = 2;
-var draftExam = [];
 var publishedExam = [];
 
 // For searching through the question bank
 function query() {
+    document.getElementById("version").innerHTML = "Version 1.0.2";
+
     var xhttp = new XMLHttpRequest();
     
     // Checks which filters are on
-    var request = "query=true";
+    var request = "query=GetQuestions";
     if(searchKeyword) {
-        request = "&keyword=" + document.getElementById("s").value;
+        request += "&question=" + document.getElementById("s").value;
     }
     if(searchTopic) {
-        request = "&topic=" + document.getElementById("s").value;
+        request += "&topic=" + document.getElementById("s").value;
     }
     if(searchDifficulty) {
-        request = "&difficulty=" + document.getElementById("s").value;
+        request += "&difficulty=" + document.getElementById("s").value;
     }
-
     xhttp.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
             var JSONresults = this.responseText;
-            var searchResults = JSON.parse(JSONresults);
+            var resultsObj = JSON.parse(JSONresults);
+            var searchResults = resultsObj.Questions;
             document.getElementById("qResults").innerHTML = "";
-            for(q = 0; q < searchResults.length; q++) {
+            for(q = 0; q < searchResults.length-1; q++) {
                 var questionName = searchResults[q].Question;
                 var questionTopic = searchResults[q].Topic;
                 var questionDifficulty = searchResults[q].Difficulty;
                 var questionID = searchResults[q].ID;
+                var questionTC = searchResults[q].TestCases;
+                /*var end = questionName + questionTopic + questionDifficulty + questionID;
+                for(p = 0; p < questionTC.length; p++) {
+                    end+=questionTC[p];
+                }*/
+                //document.getElementById("testWork").innerHTML = end;
                 document.getElementById("qResults").innerHTML += "<tr id=" + questionID + "><th>" + questionName + "</th> <th>" + questionTopic + "</th> <th>" + questionDifficulty + "</th><th><input type='text' style='visibility: hidden; width: 2rem;' id=" + questionID + "></th></tr>";
             }
         }
+        else {
+            document.getElementById("testWork").innerHTML = this.readyState + " " + this.status;
+        }
     }
-    xhttp.open("POST","frontCurl.php", true);
+    xhttp.open("POST","betaFrontCurl.php", true);
     xhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
     xhttp.send(request);
 }
@@ -54,19 +64,29 @@ function setDifficulty() {
 
 // Adding a question to the question bank
 function addQuestion() {
+    
+    // Resets the form!
     if(document.getElementById("add").innerHTML == "Add Another Question") {
         document.getElementById("add").innerHTML = "Add Question";
+        document.getElementById("sysMsg").innerHTML = "";
+        document.getElementById("functionName").innerHTML = "";
         document.getElementById("q").value = "";
         document.getElementById("ans1").value = "";
         document.getElementById("ans2").value = "";
         document.getElementById("tc1").value = "";
-        document.getElementById("tc2").value = "";
+        document.getElementById("tc2").value = ""; 
         document.getElementById("top").value = "";
+        while(numCases > 2) {
+            delTC();
+        }
     }
+
+    // Sending the question over
     else {
         var question = document.getElementById("q").value;
         var difficulty = document.getElementById("diff").value;
         var topic = document.getElementById("top").value;
+        //var fName = document.getElementById("functionName").value;
         var testCases = [];
         for(i = 1; i < numCases+1; i++) {
             testCases.push("{" + document.getElementById("tc" + i).value + ":" + document.getElementById("ans" + i).value + "}");    
@@ -78,53 +98,59 @@ function addQuestion() {
         }
         else {
             var xhttp = new XMLHttpRequest();
-            var addRequest = "question=" + question + "&difficulty=" + difficulty + "&topic=" + topic + "&testCases=" + JSON.stringify(testCases);
+            var addRequest = "query=InsertQuestion&Question=" + question + /*"&functionName=" + fName + */"&Difficulty=" + difficulty + "&Topic=" + topic + "&TestCases=" + JSON.stringify(testCases);
             xhttp.onreadystatechange = function() {
                 if(this.readyState == 4 && this.status == 200) {
-                    var response = this.responseText;
-                    if(response) {
+                    var echoed = this.responseText;
+                    var response = JSON.parse(echoed);
+                    if(response.dbSuccess) {
                         document.getElementById("add").innerHTML = "Add Another Question";
                         document.getElementById("sysMsg").innerHTML = "Question added! Send another?"
-                    }
-                    else {
-                        document.getElementById("sysMsg").innerHTML = "Error sending message."
+                        query();
                     }
                 }
+                else {
+                    document.getElementById("sysMsg").innerHTML = "Error sending message."
+                }
             }
-            xhttp.open("POST","frontCurl.php", true);
+            xhttp.open("POST","betaFrontCurl.php", true);
             xhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
             xhttp.send(addRequest);
         }
     }
 }
 
-// Adding more Test Cases
+// Adding more Test Case Fields
 function addTC() {
     if(numCases<6) {
-        var nextNum = numCases + 1;
-        var nodeIn = document.createElement("th");
-        var textIn = document.createTextNode("<input type='text' name='test" + nextNum + "' id='tc" + nextNum + "'>");
-        nodeIn.appendChild(textIn);
-        nodeIn.id = "in" + nextNum;
+        numCases++;
+        var nodeIn = document.createElement("TH");
+        var boxIn = document.createElement("INPUT");
+        boxIn.type = "text";
+        boxIn.name = "test" + numCases;
+        boxIn.id = "tc" + numCases;
+        nodeIn.appendChild(boxIn);
+        nodeIn.id = "in" + numCases;
         var tableIn = document.getElementById("input");
-        var addBut = document.getElementById("addButton");
-        tableIn.insertBefore(nodeIn, addBut);
+        var addField = document.getElementById("addButton");
+        tableIn.insertBefore(nodeIn, addField);
 
-        var nodeOut = document.createElement("th");
-        var textOut = document.createTextNode("<input type='text' name='result" + nextNum + "' id='ans" + nextNum + "'>");
-        nodeOut.appendChild(textOut);
-        nodeOut.id = "out" + nextNum;
+        var nodeOut = document.createElement("TH");
+        var boxOut = document.createElement("INPUT");
+        boxOut.type = "text";
+        boxOut.name = "result" + numCases;
+        boxOut.id = "ans" + numCases;
+        nodeOut.appendChild(boxOut);
+        nodeOut.id = "out" + numCases;
         var tableOut = document.getElementById("output");
-        var delBut = document.getElementById("delButton");
-        tableOut.insertBefore(nodeOut, delBut);
-        numCases = nextNum;
+        var delField = document.getElementById("delButton");
+        tableOut.insertBefore(nodeOut, delField);
     }
 }
 
 // Deleting Current Fields
 function delTC() {
     if(numCases>2) {
-        var nextNum = numCases - 1;
         var tableIn = document.getElementById("input");
         var elementIn = document.getElementById("in" + numCases);
         tableIn.removeChild(elementIn);
@@ -132,7 +158,7 @@ function delTC() {
         var tableOut = document.getElementById("output");
         var elementOut = document.getElementById("out" + numCases);
         tableOut.removeChild(elementOut);
-        numCases = nextNum;
+        numCases--;
     }
 }
 
@@ -143,30 +169,55 @@ function examOn() {
     document.getElementById("examEdit").onclick = examOff();
     document.getElementById("makeExam").style.visibility = visible;
     for(k = 1; k < table.getElementsByTagName("tr").length; k++) {
-        table.getElementsByTagName("tr")[k].addEventListener("click", addToExam);
+        table.getElementsByTagName("tr")[k].addEventListener("click", function(){this.classList.toggle("selected")} );
     }
 }
 function examOff() {
     var table = document.getElementById("queryResults");
     document.getElementById("examEdit").innerHTML = "Make Exam";
     document.getElementById("examEdit").onclick = examOn();
+    document.getElementById("makeExam").style.visibility = hidden;
     for(m = 1; m < table.getElementsByTagName("tr").length; m++) {
-        table.getElementsByTagName("tr")[m].removeEventListener("click", addToExam);
+        table.getElementsByTagName("tr")[m].removeEventListener("click", function(){this.classList.toggle("selected")});
+    }
+    for(p = 0; p < table.getElementByClassName("selected").length; p++) {
+        table.getElementByClassName("selected")[p].classList.toggle("selected");
     }
 }
 
-function addToExam(element) {
-    
+function addToExam() {
+    var table = document.getElementById("queryResults");
+    var examQuestions = table.getElementByClassName("selected");
+    for(s = 0; s < examQuestions.length; s++) {
+        var xhttp = new XMLHttpRequest();
+        var addRequest = "query=InsertExamQuestion&QuestionID=" + examQuestions[s].id;
+        xhttp.onreadystatechange = function() {
+            if(this.readyState == 4 && this.status == 200) {
+                var echoed = this.responseText;
+                var response = JSON.parse(echoed);
+                if(response.dbSuccess) {
+                    document.getElementById("testWork").innerHTML = "Exam Posted.";
+                }
+                else {
+                    document.getElementById("testWork").innerHTML = "Error making exam."
+                }
+            }
+        }
+        xhttp.open("POST","betaFrontCurl.php", true);
+        xhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+        xhttp.send(addRequest);
+    }
 }
 
 // Loading the exam for students
 function loadExam() {
+
     if(exam.length == 0) {
         document.getElementById("fullExam").innerHTML = "Exam not posted yet. Try again later."
     }
     else {
         for(n = 0; n < publishedExam.length; n++) {
-
+            var curNode = document.createElement("p");
         }
     }
 }
