ec8e114b9725ba009eb10f10c5e2bd94adb711db
Andy M Peralta Hernandez <amp85@afsconnect2.njit.edu>
2019-03-20 01:40:16 -0400

    Passable Grading

--- middleend/middleEnd.php
+++ middleend/middleEnd.php
@@ -16,54 +16,83 @@ if (isset($username) && isset($password)){
 
 	echo $db_result;
 	curl_close($ch1);
-	//Tell me your Account Type
+	return;
+} 
 
-} else if(isset($_POST["query"])) {
+if(isset($_POST["query"])) {
 	$query = $_POST["query"]; 
-	if($query == "GetQuestions") {
-		$url = "https://web.njit.edu/~ar664/cs490/backend/getquestions.php";
-	} else if ($query == "InsertQuestion") {
-		$url = "https://web.njit.edu/~ar664/cs490/backend/insertquestion.php";
-	} else if ($query == "DeleteQuestion") {
-		$url = "https://web.njit.edu/~ar664/cs490/backend/deletequestion.php";
-	} else if ($query == "GetExam") {
-		$url = "https://web.njit.edu/~ar664/cs490/backend/getexam.php";
-	} else if ($query == "InsertExamQuestion") {
-		$url = "https://web.njit.edu/~ar664/cs490/backend/insertexamquestion.php";
-	} else if ($query == "UpdateExamQuestion") {
-		$url = "https://web.njit.edu/~ar664/cs490/backend/updateexamquestions.php";
-	} else if ($query == "DeleteExamQuestion") {
-		$url = "https://web.njit.edu/~ar664/cs490/backend/deleteexamquestion.php";
-	} else {
-		echo "ERROR: Query not found";
-		return;
-	}
+	//if($query == "GetQuestions") {
+	//	$url = "https://web.njit.edu/~ar664/cs490/backend/getquestions.php";
+	//} else if ($query == "InsertQuestion") {
+//		$url = "https://web.njit.edu/~ar664/cs490/backend/insertquestion.php";
+//	} else if ($query == "DeleteQuestion") {
+//		$url = "https://web.njit.edu/~ar664/cs490/backend/deletequestion.php";
+//	} else if ($query == "GetExam") {
+//		$url = "https://web.njit.edu/~ar664/cs490/backend/getexam.php";
+//	} else if ($query == "InsertExamQuestion") {
+//		$url = "https://web.njit.edu/~ar664/cs490/backend/insertexamquestion.php";
+//	} else if ($query == "UpdateExamQuestion") {
+//		$url = "https://web.njit.edu/~ar664/cs490/backend/updateexamquestion.php";
+//	} else if ($query == "DeleteExamQuestion") {
+//		$url = "https://web.njit.edu/~ar664/cs490/backend/deleteexamquestion.php";
+//	} else {
+//		echo "ERROR: Query not found";
+//		return;
+//	}
 
+       switch ($query){
+         case "GetQuestions":
+	   $url = "https://web.njit.edu/~ar664/cs490/backend/getquestions.php";
+           break; 
+	 case "GetExam":
+	   $url = "https://web.njit.edu/~ar664/cs490/backend/getexam.php";
+	   break;
+         case "InsertQuestion":
+           $url = "https://web.njit.edu/~ar664/cs490/backend/insertquestion.php";
+           break;
+         case "DeleteQuestion":
+           $url = "https://web.njit.edu/~ar664/cs490/backend/deletequestion.php";
+           break;
+         case "InsertExamQuestion":
+           $url = "https://web.njit.edu/~ar664/cs490/backend/insertexamquestion.php";
+           break;
+         case "UpdateExamQuestion":
+           $url = "https://web.njit.edu/~ar664/cs490/backend/updateexamquestion.php";
+           break;
+         case "DeleteExamQuestion":
+           $url = "https://web.njit.edu/~ar664/cs490/backend/deleteexamquestion.php";
+           break;
+	 default:
+	   echo "Query \"$query\" not found" . PHP_EOL;
+	   return;
+       } 
         
-	//var_dump($_POST);
-	if (isset($_POST['Answer']) == FALSE){
+	//echo "Answer is: \n" . var_dump($_POST['Answer']) . PHP_EOL;
+	//HTML form submits will sometimes submit blank strings so empty is necessary
+	if (isset($_POST['Answer']) == FALSE || empty($_POST['Answer'])){
+	        echo "I don't have an answer\n";
 		$ch1 = curl_init($url);
 		curl_setopt($ch1, CURLOPT_POST, TRUE); // store result in var
 		curl_setopt($ch1, CURLOPT_POSTFIELDS, http_build_query($_POST)); // store result in var
 		curl_setopt($ch1, CURLOPT_RETURNTRANSFER, TRUE); // store result in var 
 		$query_result = curl_exec($ch1);
-		//echo $query_result; 
+		echo $query_result; 
 		curl_close($ch1);     
 	} else {
 		//Exam grading
+		//echo "I have an Answer: " . $_POST['Answer'] . PHP_EOL;
 		if (isset($_POST['Answer']) && $query='UpdateExamQuestion'){ 
-			
-			if (!isset($_POST['Points']) || !isset($_POST['QuestionID'])) {
+			if (!isset($_POST['QuestionID']) || empty($_POST['QuestionID'])) {
 				//! Missing required post params
-				echo "Points:" . $_POST['Points'] . PHP_EOL, 
-				     "QuestionID:" . $_POST['QuestionID'] . PHP_EOL;
-				echo 'You didn’t give me one of these Post Vars: Points and QuestionID';
+				echo "QuestionID:" . $_POST['QuestionID'] . PHP_EOL;
+				echo 'You didn’t give me a Post Var: QuestionID';
 				return;
 			}
 			
 			//Create the file from Answer
 			$answer=$_POST['Answer'];
-			$output=array();
+			//echo "This is the answer: " .$answer . PHP_EOL;
+
 
 			//execute file and mark points
 			$comments="";
@@ -80,16 +109,17 @@ if (isset($username) && isset($password)){
 			$json_QuestionLst= curl_exec($ch3);
 			$questionLst= json_decode($json_QuestionLst);
 			$arrayQuestions=$questionLst->Questions;
-		//	echo "Here are all the questions: " . var_dump($arrayQuestions)  . PHP_EOL;	
+		      //echo "Here are all the questions: " . var_dump($arrayQuestions)  . PHP_EOL;	
 			curl_close($ch3);
 		       
 			//Get functionName and TestCases from GetQuestions
 			foreach($arrayQuestions as $json_question => $value){
 			  //echo "Question value: " . $value->ID . PHP_EOL;
 			  if ( $value->ID == $_POST["QuestionID"]){
-	//		         echo "Here's the question with ID:" . $_POST["QuestionID"] . PHP_EOL . 
-	//			 "Question:  " . $value->Question . PHP_EOL;
+	                        //echo "Here's the question with ID:" . $_POST["QuestionID"] . PHP_EOL . 
+	                        //"Question:  " . $value->Question . PHP_EOL;
 				 $jsonTestCases=$value->TestCases;
+                               //echo "JsonTestCases is: " . var_dump($jsonTestCases) . PHP_EOL;
 				 $functName=$value->FunctionName;
 			  }
 		       }
@@ -106,12 +136,14 @@ if (isset($username) && isset($password)){
 			//echo "Here is the exam stuff: " . var_dump($examArry) . PHP_EOL;	
 			curl_close($ch4);
 
+			//Get The questions Total Point worth
 			foreach($examArry as $json_exam=>$exam_Value){
-			  echo "Question ID: " . $exam_Value->QuestionID . PHP_EOL;
+                           //echo "Question ID: " . $exam_Value->QuestionID . PHP_EOL;
 			  if ( $exam_Value->QuestionID == $_POST["QuestionID"]){
-			         echo "Question with ID: " . $_POST["QuestionID"] . 
-				 " is worth $exam_Value->Points Points" . PHP_EOL; 
+                                //echo "Question with ID: " . $_POST["QuestionID"] . 
+                                 //" is worth $exam_Value->Points Points" . PHP_EOL; 
 				 $pointsGiven=$exam_Value->Points;
+				 echo "Points is now set to: " . $pointsGiven . PHP_EOL;
 			  }
 		       }
 
@@ -134,19 +166,19 @@ if (isset($username) && isset($password)){
 			$answerFuncNameLen = $parensFirstIdx - $answerFuncNameStartIdx;
 			$answerFuncName = substr($answer, $answerFuncNameStartIdx, $answerFuncNameLen);
 			// replace the functionName in answer string
-			$answer = substr_replace($answer, "answer", $answerFuncNameStartIdx, $answerFuncNameLen);
 		
 			if ($answerFuncName != $functName) {
+				echo "Function name is incorrect So I'm taking away 10 points" . PHP_EOL;
+				echo "answerFuncName:$answerFuncName   functName:$functName" . PHP_EOL;
 				$pointsGiven -= 10;
 			}
-
+			$answer = substr_replace($answer, "answer", $answerFuncNameStartIdx, $answerFuncNameLen);
 			$pyBinPath=exec('which python');
 			$scriptPath=exec('pwd');
 			$filepath=$scriptPath . '/' . "student_answer.py"; 
 		        file_put_contents($filepath, $answer);
 
-          //              echo "This is Json: " . var_dump($jsonTestCases);
-	
+                       // echo "This is Json: " . var_dump($jsonTestCases) . PHP_EOL;	
 //			echo "Testcases below:" . var_dump($testCases) . PHP_EOL; 
 			if ($jsonTestCases == NULL) {
 	//			echo "Testcases within if statement:" . var_dump($testCases["Input"]) . PHP_EOL; 
@@ -154,38 +186,92 @@ if (isset($username) && isset($password)){
 				return;
 			}
 
+
 			//Currentyl fixing the forloop for testCases
-			foreach($jsonTestCases as $testCase) {
-				// {"input":"args", "output":"results"}
-				$expInput = $testCase; //expects an array of two args
-				$expOutput = $testCase;
-
-				echo "TestCase is: " . $testCase . PHP_EOL;
-				echo "expInput is: " . var_dump($expInput) . PHP_EOL;
-				echo "expOutput is: " . var_dump($expOutput) . PHP_EOL;
+			$inputCases=$jsonTestCases->Input;
+			$length=count($inputCases);
+			$outputCases=$jsonTestCases->Output;
+			for($i=0; $i < $length; $i++ ) {
+				$expInput = explode("," ,$inputCases[$i]); //expects an array of two args
+				$inputSize = count($expInput);
+				$expOutput = $outputCases[$i];
+				$data="\n\n#For Testing Purposes\n";
+				$temp="";
+                             
+			     // write the input Elements in student function 
+			     // so that it runs
+			      for($j=0; $j < $inputSize; $j++){
+				 if(is_numeric($expInput[$j])){
+				   $strtoInt=intval($expInput[$j]);
+				   $expInput[$j]=$strtoInt;
+				 }
+				 if (gettype($expInput[$j]) == "string"){
+				    $data.="arg$j="."\"".$expInput[$j]."\"\n";
+				    $temp.="arg$j, ";
+				  }else{ 
+				  $data.="arg$j=".$expInput[$j]."\n";
+                                  $temp.="arg$j, ";
+				  }
+				  if ($j == $inputSize - 1){
+				     echo "Temp is: $temp\n";
+				     if (strpos($answer, 'print') === FALSE){
+				     // echo "Function doesn't have a Print Statement\n";
+				      $data.="print(answer($temp))";
+				      $endingParenth = strpos($data, ", )");
+				      $doubleRightParenth = strpos($data, "))");
+				     } else{
+				    //  echo "Function has a Print Statement\n";
+				      $data.="$functName($temp)";
+				      $endingParenth = strpos($data, ", )");
+				      $doubleRightParenth = strpos($data, ")");
+				     }
+				    // echo "Data Currently contians: $data\n";
+				     $diffPos = $doubleRightParenth - $endingParenth;
+				    // echo "doubleRightPanranthese:$doubleRightParenth   endingParenth:$endingParenth  difPos:$diffPos\n"; 
+				     $data = substr_replace($data, '', $endingParenth, $diffPos);
+				  }
+                               }
+                                echo "Data after replace contains: $data" . PHP_EOL;
+			//	echo "TestCase is: " . var_dump($expInput) . PHP_EOL;
+			//	echo "expInput is: " . var_dump($expInput) . PHP_EOL;
+	     	       //       echo "expOutput is: " . var_dump($expOutput) . PHP_EOL;
+			
+			     
+				// expInput must always be an array with two arguments inside to work
+				$output=array(); 
+		                file_put_contents($filepath, $data, FILE_APPEND);
+				exec($pyBinPath . " " . "$scriptPath/student_answer.py " .  '2>&1', $output, $status);
 				
-				// expInput must always be an array with two arguments inside to work.
-				exec($pyBinPath . " " . "$scriptPath/py_exec_wrapper.py" . " " . $expInput[0] . " " . $expInput[1] . ' 2>&1', $output, $status);
-
-				$actualOutput = implode($output);
-				//echo "output is " . var_dump($output) . PHP_EOL;
+				
+				$actualOutput = $output[0]; //implode($output)
+				
+				//echo "ExpectedOutput is " . var_dump($expOutput) . PHP_EOL;
+			        //echo "Whats in Output: " . var_dump($output) . PHP_EOL;
 				//echo "Actual output is: " . $actualOutput . PHP_EOL;
-
+                                //echo "The current status is: " . $status . PHP_EOL;
 				if ($status == 1) {    
 					//! Handle case where provide answer doesn't successfully run
-					$pointsGiven = 0;
-				       //echo "Points Given: " . $pointsGiven . PHP_EOL;
+				      $pointsGiven = 0;
+				      echo "Program couldn't compile Points Given: " . $pointsGiven . PHP_EOL;
 				      $_POST['AutoComments']= implode("\n", $output);
-					return;
+				      echo "AutoComments: " . $_POST['AutoComments']. PHP_EOL;
+				      return;
 				}
 
 				if ($actualOutput != $expOutput) {
 					//! Handle case where the test case fails 
+					echo "actualOutput:$actualOutput expOutput:$expOutput".PHP_EOL;
 					$pointsGiven -= 7;
-				      $_POST['AutoComments']= implode("\n", $output);
-				      echo "testCases failed" . PHP_EOL;
+				        $_POST['AutoComments']= implode("\n", $output);
+				        echo "testCases failed" . "Current Points: " . $pointsGiven . PHP_EOL;
 				}
-			}
+
+		            //Restoring file to orginal form
+		            file_put_contents($filepath, $answer);
+
+	        }
+			
+			//Grading is done output result
 			if ($pointsGiven < 0) {
 				$pointsGiven = 0;
 			}
@@ -194,15 +280,15 @@ if (isset($username) && isset($password)){
 			}
 			
 			// We are done running the test cases so now lets send the points given.
-       			  $_POST['PointsGiven']=$pointsGiven;
-                           echo "student received " . $pointsGiven . "points";
+       			   $_POST['PointsGiven']=$pointsGiven;
+                           echo "student received " . $pointsGiven . " points" . PHP_EOL;
 
 			   $ch2 = curl_init($url);
 			   curl_setopt($ch2, CURLOPT_POST, TRUE); // store result in var
 			   curl_setopt($ch2, CURLOPT_POSTFIELDS, http_build_query($_POST)); // store result in var
 			   curl_setopt($ch2, CURLOPT_RETURNTRANSFER, TRUE); // store result in var 
 			   $updatedQuestion = curl_exec($ch2);
-		//	   echo $updatedQuestion;
+		           echo $updatedQuestion;
 			   curl_close($ch2);
 	}
 }
